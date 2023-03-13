7f99f03a15c358e029798f9df56ef099316d83cd
wqyang <wqyang@bu.edu>
2023-03-13 22:32:21 +0800

    add subscription route

--- axum-rs-demo/subscription/src/lib.rs
+++ axum-rs-demo/subscription/src/lib.rs
@@ -1,16 +1,28 @@
 use axum::{
-    routing::{get, IntoMakeService},
-    Router, Server,
+    routing::{get, post, IntoMakeService},
+    Form, Router, Server,
 };
 use hyper::server::conn::AddrIncoming;
+use serde::Deserialize;
 use std::net::TcpListener;
 
+#[derive(Deserialize, Debug)]
+struct SubscriptionForm {
+    name: String,
+    email: String,
+}
 async fn health_check() {}
+async fn subscriptions(Form(form): Form<SubscriptionForm>) {
+    println!("the params are: {:?}", form);
+}
 
 pub fn run(
     listener: TcpListener,
 ) -> Result<Server<AddrIncoming, IntoMakeService<Router>>, std::io::Error> {
-    let app = Router::new().route("/health_check", get(health_check));
+    let app = Router::new()
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscriptions));
+
     let server = axum::Server::from_tcp(listener)
         .expect("failed to start server")
         .serve(app.into_make_service());
